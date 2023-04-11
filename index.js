8bd54a2a5e418d6737fa8f21634683bef726dea5
Legion162 <mjesneakers@gmail.com>
2023-04-11 17:21:17 +0200

    first commit

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require(`express`)
 const app = express()
 app.set('trust proxy', true)
 const port =process.env.PORT || 8080
+const requestIP = require(`request-ip`)
 
+app.use(requestIP.mw())
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`)
 })
 
 app.get(`/IP`, (req, res)=>{
     res.end("Your IP Addresss is: " + req.socket.remoteAddress);
+    console.log(req.clientIp)
 })
